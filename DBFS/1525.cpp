d83b37e8c7ce34c1b9b91a6bcb0db1a205a1551c
b4sh5i <57925966+b4sh5i@users.noreply.github.com>
2024-03-11 20:49:12 +0900

    Update 1525.cpp

--- DBFS/1525.cpp
+++ DBFS/1525.cpp
@@ -1,53 +1,43 @@
 // 1525
 #include <iostream>
-#include <queue>
-#include <map>
 #include <string>
 
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    int dx[4] = {1, -1, 0, 0};
-    int dy[4] = {0, 0, 1, -1};
-    queue<string> Q;
-    string TARGET = "123456780";
-    map<string, int> vis;
-    string s1 = "";
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            char t; cin >> t;
-            s1 = s1 + t;
-        }
-    }
-    Q.push(s1);
-    vis[s1] = 0;
-    while(!Q.empty()){
-        string str = Q.front();
-        Q.pop();
-        if(str == TARGET){break;}
-        else {
-            int zero = str.find("0");
-            int x = zero / 3;
-            int y = zero % 3;
-            for(int i=0;i<4;i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if(nx < 3 && ny < 3 && nx >=0 && ny >= 0){
-                    string tmp = str;
-                    swap(tmp[nx*3+ny],tmp[x*3+y]);
-                    if(!vis.count(tmp)){
-                        vis[tmp] = vis[str]+1;
-                        Q.push(tmp); 
-                    }
-                }
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+int map[20][20];
+int vis[26];
+int N, M, ret;
+
+int check(int v1, int v2){ if(v1 > v2) return v1; return v2;}
+
+void dfs(int x, int y, int cnt){
+    ret = check(ret, cnt);
+    for(int i=0;i<4;i++){
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if(nx >= 0 && ny >= 0 && nx < M && ny < N){
+            if(vis[map[ny][nx]] == 0){
+                vis[map[ny][nx]] = 1;
+                dfs(nx, ny, cnt+1);
+                vis[map[ny][nx]] = 0;
             }
         }
     }
-    if (!vis.count(TARGET)) cout << -1;
-    else cout << vis[TARGET];
-    return 0;
 }
 
-
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cin >> N >> M;
+    for(int i=0;i<N;i++){
+        string t; cin >> t;
+        for(int j=0;j<M;j++){
+            map[i][j] = int(t[j]) - 0x40;
+        }
+    }
+    vis[map[0][0]] = 1;
+    dfs(0,0,1);
+    cout << ret;
+}
